83a8507d9aa0857c60ae38ab36987c80cd0cf7f6
ThunderGer <Jaames11@hotmail.com>
2022-03-23 02:11:41 -0600

    update branch

--- routes/user.py
+++ routes/user.py
@@ -2,6 +2,22 @@ from fastapi import APIRouter
 
 user = APIRouter()
 
+@user.get('/user/{id}')
+def get_user():
+    return {"Holis"}
+
 @user.get('/users/')
 def get_users():
+    return {"Holis"}
+
+@user.post('/users/')
+def create_user():
+    return {"Holis"}
+
+@user.put('/users/{id}')
+def update_user():
+    return {"Holis"}
+
+@user.delete('/users/{id}')
+def delete_user():
     return {"Holis"}
