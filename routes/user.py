62e80a6d631275e2873e131d81d507693d162121
unknown <creyesr1601@alumno.ipn.mx>
2022-03-28 15:18:41 -0600

    Cambios

--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,37 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Response, status
+from config.db import conn 
+from schemas.user import userEntity, usersEntity
+from models.user import User
+from passlib.hash import sha256_crypt
+from bson import ObjectId
+from starlette.status import HTTP_204_NO_CONTENT
 user = APIRouter()
 
-@user.get('/users/')
-def get_users():
+@user.get('/users', response_model= list[User], tags=['User'])
+def get_user():
+    return userEntity(conn.local.user.find())
+
+@user.post('/user', response_model=list [User], tags=['Users'])
+def create_users(user: User):
+    new_user =dict(user)
+    new_user['password']= sha256_crypt.encrypt(new_user['password'])
+    del new_user['id']
+    id = conn.local.user.insert_one(new_user).inserted_id
+    user= conn.local.user.find_one({"_id" : id})
+    return userEntity(user)
+
+@user.post('/user/{id}', response_model=User, tags=['Users'])
+def find_user(id: str):
+    return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
+
+@user.post('/users/')
+def create_user():
+    return {"Holis"}
+
+@user.put('/users/')
+def update_user():
+    return {"Holis"}
+
+@user.delete('/users/')
+def delete_user():
     return {"Holis"}
