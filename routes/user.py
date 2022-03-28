f585b72f108ed5c988d05e3c7528aeacbc6c9815
Sanmiri11 <msanchezm1502@alumno.ipn.mx>
2022-03-28 15:17:54 -0600

    Agregamos models y modificamos rutas del usuario.

--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,39 @@
-from fastapi import APIRouter
+from curses.ascii import US
+from fastapi import APIRouter, Response, status
+from config.bd import conn
+from schemas.user import userEntity, usersEntity
+from models.user import User
+from passlib.hash import sha256_crypt
+from bson import ObjectId
+from starlette.status import HTTP_204_NO_CONTENT
 
 user = APIRouter()
 
-@user.get('/users/')
+@user.get('/users', response_model= list[User], tags= ['User'])
 def get_users():
+    return userEntity(conn.local.user.find())
+
+@user.post('/users/', response_model= list[User], tags =['Users'])
+def create_users(user: User):
+    new_user = dict(user)
+    new_user['password'] = sha256_crypt.encrypt(new_user['password'])
+    del new_user['id']
+    id = conn.local.user.insert_one(new_user).inserted_id
+    user = conn.local.user.find_one({"_id" : id})
+    return userEntity(user)
+
+@user.get('/user/{id}', response_model= User, tags= ['Users'])
+def find_user(id: str):
+    return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
+
+@user.post('/users/')
+def create_user():
+    return {"Holis"}
+
+@user.put('/users/{id}')
+def update_user():
+    return {"Holis"}
+
+@user.delete('/users/{id}')
+def delete_user():
     return {"Holis"}
