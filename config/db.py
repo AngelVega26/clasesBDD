82c7f258de6d8f0723fd5da6ad687dc67123e37f
unknown <creyesr1601@alumno.ipn.mx>
2022-03-29 14:57:31 -0600

    proyecto terminado

--- config/db.py
+++ config/db.py
@@ -1,4 +1,5 @@
 from pymongo import  MongoClient
 
 mongo_url = 'mongodb://mongo:iPAnHlBPYNyPlsoIltgc@containers-us-west-15.railway.app:7323'
-conn == MongoClient(mongo_url)
+
+conn = MongoClient(mongo_url)
--- routes/user.py
+++ routes/user.py
@@ -1,38 +1,39 @@
 from fastapi import APIRouter, Response, status
-from config.db import conn 
+from config.db import conn
 from schemas.user import userEntity, usersEntity
 from models.user import User
 from passlib.hash import sha256_crypt
 from bson import ObjectId
 from starlette.status import HTTP_204_NO_CONTENT
+
 user = APIRouter()
 
-@user.get('/users', response_model= list[User], tags=['User'])
-def get_user():
-    return userEntity(conn.local.user.find())
+@user.get('/users', response_model=list[User], tags=['Users'])
+def get_users():
+    return usersEntity(conn.local.user.find())
 
-@user.post('/user', response_model=list [User], tags=['Users'])
-def create_users(user: User):
-    new_user =dict(user)
-    new_user['password']= sha256_crypt.encrypt(new_user['password'])
+@user.post('/users', response_model=User, tags=['Users'])
+def create_user(user: User):
+    new_user = dict(user)
+    new_user['password'] = sha256_crypt.encrypt(new_user['password'])
     del new_user['id']
     id = conn.local.user.insert_one(new_user).inserted_id
-    user= conn.local.user.find_one({"_id" : id})
+
+    user = conn.local.user.find_one({"_id": id})
     return userEntity(user)
 
-@user.post('/user/{id}', response_model=User, tags=['Users'])
+@user.get('/users/{id}', response_model=User, tags=['Users'])
 def find_user(id: str):
     return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
 
-
 @user.put('/users/{id}', response_model=User, tags=['Users'])
-def update_user(id: str, user: User):
+def update_user(id : str, user: User):
     new_user = dict(user)
     new_user['password'] = sha256_crypt.encrypt(new_user['password'])
     conn.local.user.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict(new_user)})
-    return userEntity (conn.local.user.find_one({"_id": ObjectId(id)}))
+    return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
 
 @user.delete('/users/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['Users'])
 def delete_user(id: str):
     userEntity(conn.local.user.find_one_and_delete({"_id": ObjectId(id)}))
-    return Response(satus_code=HTTP_204_NO_CONTENT)
+    return Response(status_code=HTTP_204_NO_CONTENT)
